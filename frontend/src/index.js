b47dd6469ad8e66a9ba6abb4aff3348b5378715b
Ryan <ryan.merrill2004@yahoo.com>
2025-05-23 22:03:50 -0400

    created index.js to hopefully fix the deployment error in Vercel

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // ✅ Required for <Link>
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
