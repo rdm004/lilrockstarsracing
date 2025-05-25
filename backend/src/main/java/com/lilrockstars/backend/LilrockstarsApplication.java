33b642d222369a1e9356eed51197fcd84c9ffa71
Ryan <ryan.merrill2004@yahoo.com>
2025-05-25 00:19:23 -0400

    Updated application.properties again

--- backend/src/main/java/com/lilrockstars/backend/LilrockstarsApplication.java
+++ backend/src/main/java/com/lilrockstars/backend/LilrockstarsApplication.java
@@ -12,20 +12,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class LilrockstarsApplication {
     public static void main(String[] args) {
         // Load variables from .env file
-        Dotenv dotenv = Dotenv.configure()
-                .directory("./")  // directory where .env is located
-                .filename(".env") // name of the env file
-                .load();
-
-        // Inject into system properties so Spring can use them
-        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-
-        // Debug print
-        System.out.println("SPRING_DATASOURCE_URL = " + dotenv.get("SPRING_DATASOURCE_URL"));
-        System.out.println("SPRING_DATASOURCE_USERNAME = " + dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.out.println("SPRING_DATASOURCE_PASSWORD = " + dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+//        Dotenv dotenv = Dotenv.configure()
+//                .directory("./")  // directory where .env is located
+//                .filename(".env") // name of the env file
+//                .load();
+//
+//        // Inject into system properties so Spring can use them
+//        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+//        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+//        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+//
+//        // Debug print
+//        System.out.println("SPRING_DATASOURCE_URL = " + dotenv.get("SPRING_DATASOURCE_URL"));
+//        System.out.println("SPRING_DATASOURCE_USERNAME = " + dotenv.get("SPRING_DATASOURCE_USERNAME"));
+//        System.out.println("SPRING_DATASOURCE_PASSWORD = " + dotenv.get("SPRING_DATASOURCE_PASSWORD"));
 
         SpringApplication.run(LilrockstarsApplication.class, args);
     }
