972e01f53fbd134b699f3a7201ece5eaf7db4579
Ryan <ryan.merrill2004@yahoo.com>
2025-05-26 00:01:14 -0400

    fixed vite.config.js in new media branch.

--- frontend/media-react/vite.config.js
+++ frontend/media-react/vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  })
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080',
+    },
+  },
+});
