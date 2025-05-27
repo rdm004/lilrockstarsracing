ab0b16ed7928c30168dedb422cdb091b1e92a032
Ryan <ryan.merrill2004@yahoo.com>
2025-05-27 15:03:19 -0400

    403 error SecurityConfig

--- backend/src/main/java/com/lilrockstars/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/lilrockstars/backend/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://your-production-site.com")
+                        .allowedOrigins("http://localhost:3000", "https://lilrockstarsracing-admin.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
