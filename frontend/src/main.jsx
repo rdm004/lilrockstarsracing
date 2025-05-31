d20078485f6719b8b4f7ecd6fe522bf399059bc8
Ryan <ryan.merrill2004@yahoo.com>
2025-05-31 13:48:36 -0400

    had to import css files to get correct setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css'; // Optional global styles
 
+import '/components/global.css';
+import '/components/animations.css';
+import '/components/hero-footer.css';
+import '/components/pages.css';
+import '/components/responsive.css';
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
